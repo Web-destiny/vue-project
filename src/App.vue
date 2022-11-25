8ab12982e03e7485d1b9108bff1696a8bfaf6b92
Kuroro <takehiko.suzume@yandex.ru>
2022-11-25 17:48:58 +0300

    Подверстал поиск пива (стили)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
-
-    <h1>Поиск по списку:</h1>
-    <input type="text" v-model="search" placeholder="Искать по названию пива..">
-    <ul>
-      <li v-for="beer of sortedBeers"><b>Название пива:</b> {{ beer.name }} <b>Цена:</b> {{ beer.price }}</li>
-    </ul>
+    <div class="searchBox">
+      <h1 class="searchBox-title">Поиск пива:</h1>
+      <input type="text" v-model="searchBeer" class="search-input" placeholder="Искать по названию пива..">
+    </div>
+    <div class="beerBox">
+      <div v-for="beer of sortedBeers" class="beerCard">
+        <div style="display: flex;flex-direction: column;gap: 3px;height: 100px;">
+          <div>Бренд пива:</div>
+          <div style="font-size: 18px;font-weight: bold;color: #a51409;">{{ beer.name }}</div>
+          <div style="display: flex;align-items: flex-end;justify-content: space-between;">
+            <div>Цена:</div>
+            <div style="font-size: 25px;color: #e81000;font-weight: bold;">{{ beer.price }}</div>
+          </div>
+        </div>
+        <img :src="beer.image" alt="Картинка ушла нахуй))">
+      </div>
+    </div>
 
   </div>
 </template>
@@ -18,14 +29,14 @@ export default {
     return {
       title: 'Hello, i am Vue!',
       beers: [],
-      search: ''
+      searchBeer: ''
     }
   },
   computed: {
     sortedBeers(){
       return this.beers.filter((beer) => {
         let nameBeer = beer.name.toLowerCase()
-        return nameBeer.indexOf(this.search.toLowerCase()) !== -1
+        return nameBeer.indexOf(this.searchBeer.toLowerCase()) !== -1
       })
     }
   },
@@ -45,6 +56,7 @@ export default {
     fetch('https://api.sampleapis.com/beers/ale')
       .then( response => response.json() )
       .then( beers => this.beers = beers )
+
   }
 }
 </script>
@@ -56,7 +68,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   padding-left: 20px;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #ffffff;
 }
 
 h1, h2 {
@@ -75,5 +87,54 @@ li {
 
 a {
   color: #42b983;
+}
+
+
+.beerBox{
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: center;
+  padding: 10px;
+}
+
+.beerCard {
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+  border: 1px solid #adb9ff;
+  border-radius: 8px;
+  padding: 20px;
+  width: 235px;
+  height: 240px;
+  background: linear-gradient(to top left,#0030ff, #a7b7ff, #bcc5ff);
+  box-shadow: 4px 2px 8px;
+  cursor: pointer;
+}
+
+.beerCard img{
+  max-width: 90px;
+  max-height: 130px;
+}
+.searchBox {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px;
+}
+.searchBox-title {
+  font-size: 50px;
+  font-family: fantasy;
+  color: royalblue;
+}
+
+.search-input {
+  width: 300px;
+  height: 30px;
+  padding: 5px 15px;
+  font-size: 18px;
+  border: 2px solid royalblue;
+  font-family: fangsong;
+  box-shadow: 1px 1px 1px royalblue;
 }
 </style>
