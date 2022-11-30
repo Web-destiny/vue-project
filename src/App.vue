3ab8abd6c21aff5b8f8aa34a801b1ea3d8d5a411
Kuroro <takehiko.suzume@yandex.ru>
2022-11-30 11:37:15 +0300

    Кастомный контрол

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,56 @@
 <template>
   <div id="app">
 
+      <input type="text" v-model="title">
       <h1>{{ title }}</h1>
+
+      <textarea v-model="paragraph">{{ paragraph }}</textarea>
+      <p class="par">{{paragraph}}</p>
+
+    <label>
+    <input type="checkbox" value="vk" v-model="socialCheckBox"> VK
+  </label>
+    <label>
+      <input type="checkbox" value="facebook" v-model="socialCheckBox"> FACEBOOK
+    </label>
+    <label>
+      <input type="checkbox" value="tg" v-model="socialCheckBox"> TELEGRAM
+    </label>
+
+    <ul>
+      <li v-for="s in socialCheckBox">{{ s }}</li>
+    </ul>
+
+
+    <label>
+      <input type="radio" value="vk" v-model="socialRadio"> VK
+    </label>
+    <label>
+      <input type="radio" value="facebook" v-model="socialRadio"> FACEBOOK
+    </label>
+    <label>
+      <input type="radio" value="tg" v-model="socialRadio"> TELEGRAM
+    </label>
+
+      <p>{{ socialRadio }}</p>
+
+    <select v-model="selectedDrink">
+      <option v-for="d in drinks">{{ d }}</option>
+    </select>
+
+    <p>{{ selectedDrink }}</p>
+
+    <input type="text" v-model.number="age">
+    <p>{{ age }}</p>
+
+    <app-onoff v-model="switched"></app-onoff>
+
+    <div>
+      <h3 v-if="switched">Контрол включен</h3>
+      <h3 v-else>Контрол выключен</h3>
+    </div>
+
+
 
 
   </div>
@@ -9,13 +58,31 @@
 
 <script>
 
+import Onoff from "./Onoff";
+
 export default {
   name: 'app',
   data () {
     return {
       title: 'Hello, i am Vue!',
+      paragraph: 'Какой-то параграф..',
+      socialCheckBox: ['vk'],
+      socialRadio: '',
+      drinks: ['Coca', 'Sprite', 'Pepsi', 'Fanta'],
+      selectedDrink: 'Coca',
+      age: 24,
+
+      switched: false,
     }
-  }
+  },
+  components: {
+    appOnoff: Onoff
+  },
+  watch: {
+      age(value){
+        console.log(value)
+      }
+    }
 }
 </script>
 
@@ -45,6 +112,9 @@ li {
 
 a {
   color: #42b983;
+}
+.par{
+  white-space: pre;
 }
 
 
---
+++ src/Onoff.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+
+    <h1>Custom control</h1>
+    <button
+      class="on"
+      :class="{'active': value}"
+      @click="onChange(true)"
+    >On</button>
+
+    <button
+      class="off"
+      :class="{'active': !value}"
+      @click="onChange(false)"
+    >Off</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Onoff",
+  props: [ 'value' ],
+
+  methods: {
+    onChange(newValue) {
+        this.$emit('input', newValue)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  .on, .off{
+    background-color: #e8e6e6;
+    color: black;
+    border: none;
+    padding: 15px 35px;
+    cursor: pointer;
+    font-size: 20px;
+  }
+  .on.active{
+    background-color: #49a832;
+    color: white;
+  }
+  .off.active{
+    background-color: #c80a4e;
+    color: white;
+  }
+  button:active{
+    box-shadow: 1px 2px 3px #bec6cd;
+  }
+
+</style>
