5646c593eaac297939f3888cf266c431060f60b1
Kuroro <takehiko.suzume@yandex.ru>
2022-12-05 20:22:37 +0300

    Все почистил

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,15 @@
 <template>
   <div class="container">
-    <nav class="navbar navbar-expand-lg bg-light">
-      <div class="container-fluid">
-        <div class="collapse navbar-collapse">
-          <ul class="navbar-nav">
-<!--            <li class="nav-item">-->
-<!--              <router-link class="nav-link" exact aria-current="page" to="/">Home</router-link>-->
-<!--            </li>-->
-<!--            <li class="nav-item">-->
-<!--              <router-link class="nav-link" to="/cars">Cars</router-link>-->
-<!--            </li>-->
-            <router-link tag="li" exact active-class="active" to="/">
-              <a class="nav-link">Home</a>
-            </router-link>
-            <router-link tag="li" active-class="active" to="/cars">
-              <a class="nav-link">Cars</a>
-            </router-link>
 
-            <router-link tag="li" exact active-class="active" to="/car/4">
-              <a class="nav-link">Car 4</a>
-            </router-link>
-            <router-link tag="li" active-class="active" to="/car/5">
-              <a class="nav-link">Car 5</a>
-            </router-link>
-
-
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-
-    <router-view></router-view>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'app',
   data () {
     return {
-      title: 'Hello Vue',
+
     }
   },
 }
@@ -50,9 +18,6 @@ export default {
 <style scoped>
  .container{
    padding-top: 30px;
- }
- .active{
-    font-weight: 500;
  }
 
 </style>
