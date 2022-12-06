1120f531dfcade963b6e4175ee21efe235934b3c
Kuroro <takehiko.suzume@yandex.ru>
2022-12-06 18:04:53 +0300

    Создал интерсептор для request

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import VueResourсe from 'vue-resource'
 import App from './App.vue'
 
 Vue.use(VueResourсe)
-
 Vue.http.options.root = 'http://localhost:3000/'
+Vue.http.interceptors.push(request => {
+
+  request.headers.set('Authentification', 'UNIQUE TOKEN: ' + Math.random())
+
+})
 
 new Vue({
   el: '#app',
